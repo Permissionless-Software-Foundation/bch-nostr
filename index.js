cb311692748ff09326ec46c22eae1d5ce22aab2e
Chris Troutner <chris.troutner@gmail.com>
2024-11-18 05:42:39 -0800

    fix(sendMsgSignal()): Added signal lib & sendMsgSignal() function

--- index.js
+++ index.js
@@ -5,20 +5,15 @@
 
 /* eslint-disable no-async-promise-executor */
 
-import BCHJS from '@psf/bch-js'
-
-import Util from './lib/util.js'
-
 import Keys from './lib/keys.js'
 import Post from './lib/post.js'
-const util = new Util()
+import Signal from './lib/signal.js'
 
 class BchNostr {
   constructor () {
-    this.bchjs = new BCHJS()
-    this.util = util
     this.keys = new Keys()
     this.post = new Post()
+    this.signal = new Signal()
   }
 }
 
